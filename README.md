eb9b9d8c7a6ec0f4cf6fae58becde0c459d7668e
Rylan <rylanschaeffer@gmail.com>
2025-05-08 08:17:26 -0700

    Pushing results

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # KoyejoLab-Chinchilla-Re-Analysis
+
+## Installation
+
+1. (Optional) Update conda:
+
+`conda update -n base -c defaults conda -y`
+
+2. Create and activate the conda environment:
+
+`conda create -n chinchilla_env python=3.11 -y && conda activate chinchilla_env`
+
+3. Install the required packages:
+
+`pip install pandas seaborn`
---
+++ notebooks/00_correcting_parameters/00_correcting_parameters.py
@@ -0,0 +1,112 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+import os
+
+import numpy as np
+import pandas as pd
+
+import src.analyze
+import src.plot
+
+
+refresh = False
+# refresh = True
+
+data_dir, results_dir = src.analyze.setup_notebook_dir(
+    notebook_dir=os.path.dirname(os.path.abspath(__file__)),
+    refresh=False,
+)
+
+chinchilla_parameters_df = pd.read_csv("data/chinchilla_model_parameters.csv")
+chinchilla_parameters_df["Reported Parameters"] = (
+    1e6 * chinchilla_parameters_df["Reported Parameters (M)"]
+)
+chinchilla_parameters_df["Incorrect Eqn. Parameters"] = (
+    1e6
+    * chinchilla_parameters_df[
+        "Tied (Un)Embed, No Gating, Incorrect Attn Params Prefactor"
+    ]
+)
+chinchilla_parameters_df["Correct Eqn. Parameters"] = (
+    1e6 * chinchilla_parameters_df["Tied (Un)Embed, No Gating"]
+)
+chinchilla_parameters_df["Relative Error Correct Eqn."] = (
+    100.0
+    * np.abs(
+        chinchilla_parameters_df["Reported Parameters"]
+        - chinchilla_parameters_df["Correct Eqn. Parameters"]
+    )
+    / chinchilla_parameters_df["Reported Parameters"]
+)
+chinchilla_parameters_df["Relative Error Incorrect Eqn."] = (
+    100.0
+    * np.abs(
+        chinchilla_parameters_df["Reported Parameters"]
+        - chinchilla_parameters_df["Incorrect Eqn. Parameters"]
+    )
+    / chinchilla_parameters_df["Reported Parameters"]
+)
+
+plt.close()
+plt.figure(figsize=(10, 6))
+g = sns.scatterplot(
+    data=chinchilla_parameters_df,
+    x="Reported Parameters",
+    y="Relative Error Correct Eqn.",
+    label="Correct Eqn.",
+)
+sns.scatterplot(
+    data=chinchilla_parameters_df,
+    x="Reported Parameters",
+    y="Relative Error Incorrect Eqn.",
+    label="Incorrect Eqn.",
+)
+g.set(
+    xscale="log",
+    ylabel="Relative Error (\%)",
+)
+sns.move_legend(g, "upper left", bbox_to_anchor=(1, 1))
+src.plot.save_plot_with_multiple_extensions(
+    plot_dir=results_dir,
+    plot_filename="y=relative_error_x=reported_parameters_hue=equation",
+)
+plt.show()
+
+plt.close()
+plt.figure(figsize=(10, 6))
+sns.scatterplot(
+    data=chinchilla_parameters_df,
+    x="Reported Parameters",
+    y="Correct Eqn. Parameters",
+    label="Correct Eqn.",
+)
+g = sns.scatterplot(
+    data=chinchilla_parameters_df,
+    x="Reported Parameters",
+    y="Incorrect Eqn. Parameters",
+    label="Incorrect Eqn.",
+)
+plt.plot(
+    [3.16e7, 3.16e10],
+    [3.16e7, 3.16e10],
+    color="black",
+    linestyle="--",
+)
+g.set(
+    xlim=(3.16e7, 3.16e10),
+    ylim=(3.16e7, 3.16e10),
+    xlabel="Reported Parameters",
+    ylabel="Calculated Parameters",
+    xscale="log",
+    yscale="log",
+)
+sns.move_legend(g, "upper left", bbox_to_anchor=(1, 1))
+src.plot.save_plot_with_multiple_extensions(
+    plot_dir=results_dir,
+    plot_filename="y=calculated_parameters_x=reported_parameters_hue=equation",
+)
+plt.show()
+
+
+print()
